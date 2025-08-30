f83d04909a34032bd53247701aa70968a5f10de3
SungminLeem <zzangism@gmail.com>
2025-08-30 13:30:04 -0400

    Implement PostgreSQL DB connection and User model with CRUD API

---
+++ server/models/User.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../db");
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "users",
+    timestamps: true, // createdAt, updatedAt 자동 생성
+  }
+);
+
+module.exports = User;
